67cc96f24d03c34de33111169eb02eff3da0bf58
Alexander M. Artamonov <art.alex.m@gmail.com>
2018-07-26 23:49:33 +0300

    Задание 5

---
+++ task5.sql
@@ -0,0 +1,15 @@
+-- Написать SQL запрос, который выведет все пропуски
+-- исходные данные
+create table test (
+  id int not null primary key
+);
+
+insert into test (id) values (1), (2), (3), (6), (8), (9), (12);
+
+-- запрос выборки интервалов пропусков
+select id as "FROM", next_id as "TO"
+from (
+  select id, lead(id) over(order by Id) as next_id
+  from test
+) T
+where id + 1 <> next_id;
